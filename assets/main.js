b9d94f1ffbe6dce40eabab435710d69ed30fe89c
致远 <pfsjdsy@users.noreply.github.com>
2026-09-21 16:52:59 +0800

    调整顺序

--- assets/main.js
+++ assets/main.js
@@ -71,7 +71,7 @@
           "</div>" +
           '<div class="footer-col">' +
             "<h4>栏目</h4>" +
-            '<a href="tax.html">财务</a><a href="law.html">法律</a><a href="finance.html">税法</a><a href="share.html">转载</a>' +
+            '<a href="finance.html">财务</a><a href="law.html">法律</a><a href="tax.html">税法</a><a href="share.html">转载</a>' +
           "</div>" +
           '<div class="footer-col">' +
             "<h4>关于</h4>" +
